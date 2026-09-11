bc80d19f11bb0de5402ae8e1d74478f4cdf7828e
kazukitakemori <kazuki.takemori.sub@gmail.com>
2026-09-12 01:14:40 +0900

    Load second-life consistency styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import {
   LicenseKeyAdminPage,
 } from './components/license/LicenseKeyAdminPage';
 import './index.css';
+import './secondLifeConsistency.css';
 import './mobile.css';
 
 const root = createRoot(document.getElementById('root')!);
