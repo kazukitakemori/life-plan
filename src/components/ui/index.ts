583d77935623b9708c5d02e8bb5d3c983e6f5cbd
kazukitakemori <kazuki.takemori.sub@gmail.com>
2026-09-13 21:49:45 +0900

    Export shared copy settings control

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -5,3 +5,4 @@ export { FormChoice } from './FormChoice';
 export { DisclosureSection } from './DisclosureSection';
 export { AmountInput } from './AmountInput';
 export { StepHeading } from './StepHeading';
+export { CopySettingsBar } from './CopySettingsBar';
