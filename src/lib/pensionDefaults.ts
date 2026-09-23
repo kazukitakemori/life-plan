44001e02c927b2cd05f5e8ea05186508dc7475d1
kazukitakemori <kazuki.takemori.sub@gmail.com>
2026-09-23 16:41:10 +0900

    Q8 定期便月別入力の納付状況を既定値へ追加

--- src/lib/pensionDefaults.ts
+++ src/lib/pensionDefaults.ts
@@ -71,6 +71,7 @@ function createDefaultRecentMonthlyInputRow(): TeikibinRecentMonthlyInputRow {
     employeesPensionCategory: '',
     standardRemuneration: '',
     standardBonus: '',
+    premiumPayment: '',
   };
 }
 
